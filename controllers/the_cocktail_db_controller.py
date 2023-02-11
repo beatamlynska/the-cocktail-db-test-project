349c64702691162ba161468732082c47b76a8198
Beata Mlynska <bettymlynska@gmail.com>
2023-02-11 21:19:53 +0100

    CR fix

--- controllers/the_cocktail_db_controller.py
+++ controllers/the_cocktail_db_controller.py
@@ -2,14 +2,21 @@ import requests
 
 
 class TheCocktailDbController:
-    baseUrl = 'https://www.thecocktaildb.com/'
     headers = {'accept': 'application/json', 'Content-Type': 'application/json'}
 
-    def get_a_list_of_cocktails_by_first_letter(self, letter: str) -> (object, dict):
-        try:
-            url = self.baseUrl + f'api/json/v1/1/search.php?f={letter}'
-            response = requests.get(url, headers=self.headers)
+    def __init__(self):
+        self.base_url = 'https://www.thecocktaildb.com'
+        self.get_a_list_of_cocktails_by_first_letter_url = self.base_url + "/api/json/v1/1/search.php?f={}"
+
+    def get_response_from_get_a_list_of_cocktails_by_first_letter_endpoint(self, letter: str):
+        response = requests.get(self.get_a_list_of_cocktails_by_first_letter_url.format(letter))
+        return response
+
+    def get_a_list_of_cocktails_by_first_letter(self, letter: str) -> dict:
+        response = self.get_response_from_get_a_list_of_cocktails_by_first_letter_endpoint(letter)
+        if response.status_code == 200:
             drink_list = response.json()['drinks']
-        except requests.exceptions.JSONDecodeError:
-            return response
-        return response, drink_list
+            return drink_list
+        else:
+            print("Something went wrong!")
+
--- tests/test_the_cocktail_db_controller.py
+++ tests/test_the_cocktail_db_controller.py
@@ -6,36 +6,28 @@ import pytest
 class TestTheCocktailDbController:
     controller = TheCocktailDbController()
 
-    # Positive case, drinks are returned
-    def test_when_drink_for_given_letter_exists_then_return_list_of_drinks(self):
-        response, drink_list = self.controller.get_a_list_of_cocktails_by_first_letter('a')
+    # Positive case - drinks are returned
+    def test_when_drink_for_given_letter_exists_then_returns_only_list_of_drinks_start_with_this_letter(self):
+        drink_list = self.controller.get_a_list_of_cocktails_by_first_letter('a')
 
-        # This condition is checking if all drinks started on 'a' letter
         for item in drink_list:
             drink_name = item['strDrink']
             assert drink_name[0].lower().startswith("a")
 
-        # This condition is checking if response code is as expected
-        assert response.status_code == 200
-
-    # Positive Case, but empty response excepted
+    # Positive case - empty response excepted
     def test_when_drink_for_given_letter_does_not_exist_then_return_null_in_drinks_list(self):
-        response, drink_list = self.controller.get_a_list_of_cocktails_by_first_letter('x')
+        drink_list = self.controller.get_a_list_of_cocktails_by_first_letter('x')
 
-        # assertion for response: {"drinks":null}
         assert drink_list is None
 
-        # This condition is checking if response code is as expected
-        assert response.status_code == 200
-
-    # Negative case, exception expected
+    # Negative case - exception expected, status code should not be 200.
     def test_when_get_drink_for_given_letter_with_incorrect_input_then_response_is_empty(self):
-        response = self.controller.get_a_list_of_cocktails_by_first_letter('ax')
+        response = self.controller.get_response_from_get_a_list_of_cocktails_by_first_letter_endpoint('ax')
 
-        # We excepting error here, because this response does not return any json object.
+        # This assertion make sense only if we expecting no json in response. This should be clarified.
         with pytest.raises(requests.exceptions.JSONDecodeError):
             response.json()
 
-        # This condition is checking if response code is as expected
-        assert response.status_code == 200
+        # In this assertion, we expecting that response code for incorrect input should not be 200.
+        assert response.status_code is not 200
 
